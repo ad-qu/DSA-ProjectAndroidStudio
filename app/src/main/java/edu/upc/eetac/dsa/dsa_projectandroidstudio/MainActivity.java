f66e388e67cb86ebcc9f7bf3e23cd996ed792eaf
ad-qu <adquiros15@outlook.com>
2022-05-14 19:16:25 +0200

    new activity added

--- app/src/main/java/edu/upc/eetac/dsa/dsa_projectandroidstudio/MainActivity.java
+++ app/src/main/java/edu/upc/eetac/dsa/dsa_projectandroidstudio/MainActivity.java
@@ -2,8 +2,11 @@ package edu.upc.eetac.dsa.dsa_projectandroidstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton go_btn = (ImageButton) findViewById(R.id.imageButtonGo);
+        TextView user_text = (TextView) findViewById(R.id.editTextUsername);
+        TextView password_text = (TextView) findViewById(R.id.editTextPassword);
+
+        go_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //IF USUARIO INICIA SESIÓN
+                openMenuActivity();
+            }
+        });
+    }
+
+    private void openMenuActivity() {
+
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
